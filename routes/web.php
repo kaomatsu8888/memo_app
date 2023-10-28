ad73cef6b6530d950e9b1d3095653c2c0d1b4360
kaomatsu8888 <m.kaoru0830@gmail.com>
2023-10-28 16:42:58 +0900

    ルーティング定義

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MemoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// リソースを使用する場合//1定義で7つのルーティングが定義できる
+Route::resource('memos', MemoController::class);
+
+// リソースを使用しない場合↓が必要
+// Route::get('/memos', [MemoController::class, 'index']);//indexメソッドを呼び出す
+// Route::get('/memos/create', [MemoController::class, 'create']);//createメソッドは
+// Route::post('/memos', [MemoController::class, 'store']);//postメソッドはデータを追加する
+
+// Route::get('/memos/{memo}', [MemoController::class, 'show']);
+// Route::get('/memos/{memo}/edit', [MemoController::class, 'edit']);
+// Route::patch('/memos/{memo}', [MemoController::class, 'update']);
+// Route::delete('/memos/{memo}', [MemoController::class, 'destroy']);
+
